3f0696a82d6bbdd4499c50727f697eb23b2a2485
André Cocuroci <andrec@gazetadopovo.com.br>
2014-02-25 17:14:36 -0300

    teste

--- BoaViagem/src/br/com/casadocodigo/boaviagem/DashboardActivity.java
+++ BoaViagem/src/br/com/casadocodigo/boaviagem/DashboardActivity.java
@@ -1,11 +1,13 @@
 package br.com.casadocodigo.boaviagem;
 
-import android.os.Bundle;
+import android.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.TextView;
+//import android.widget.Toast;
 
 public class DashboardActivity extends Activity {
 
@@ -23,9 +25,15 @@ public class DashboardActivity extends Activity {
 	}
 	
 	public void selecionarOpcao(View view) {
-		TextView textView = (TextView) view;
-		String opcao = "Opção: " + textView.getText().toString();
-		Toast.makeText(this, opcao, Toast.LENGTH_SHORT).show();
+//		TextView textView = (TextView) view;
+//		String opcao = "Opção: " + textView.getText().toString();
+//		Toast.makeText(this, opcao, Toast.LENGTH_SHORT).show();		
+		
+		switch (view.getId()) {
+			case R.id.nova_viagem:
+				startActivity(new Intent(this, ViagemActivity.class));
+				break;
+		}
+		
 	}
-
 }
